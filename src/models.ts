bdd9b57d34995910b518bceadfacf481b9f06e4e
Ajordat <alexjortri@gmail.com>
2026-01-25 18:41:12 +0100

    Add new sets (duster, dustertop, dustercb, dusterop) + new rarity (#7)
    * Add new sets (duster, dustertop, dustercb, dusterop) + new rarity
    
    * Modify rarity size based on a list of sets
    
    * Fix validation of array content

--- src/models.ts
+++ src/models.ts
@@ -21,30 +21,50 @@ export enum RefRarity {
   Rare = "R1",    // 1
   RareOOF = "R2", // 2
   Unique = "U",   // 3
+  Exalt = "E",    // 4
 }
 
 export enum RefSetCode {
   // 0 = invalid
-  CoreKS = "COREKS",       // 1
-  Core = "CORE",           // 2
-  Alize = "ALIZE",         // 3
-  Bise = "BISE",   	       // 4
-  TumultS3 = "TCS3",       // 5
-  WCQualifier25 = "WCQ25", // 6
-  WCSeries25 = "WCS25",    // 7
-  Cyclone = "CYCLONE",     // 8
+  CoreKS = "COREKS",        // 1
+  Core = "CORE",            // 2
+  Alize = "ALIZE",          // 3
+  Bise = "BISE",   	        // 4
+  TumultS3 = "TCS3",        // 5
+  WCQualifier25 = "WCQ25",  // 6
+  WCSeries25 = "WCS25",     // 7
+  Cyclone = "CYCLONE",      // 8
+  Duster = "DUSTER",        // 9
+  DusterTOP = "DUSTERTOP",  // 10
+  DusterCB = "DUSTERCB",    // 11
+  DusterOP = "DUSTEROP",    // 12
 }
 
 export const SetCodeIdBitLengthMap: Record<number, number> = {
-  1: 5, // CoreKS        range 0-31
-  2: 5, // Core          range 0-31
-  3: 6, // Alize         range 0-63
-  4: 6, // Bise          range 0-63
-  5: 6, // TumultS3      range 0-63
-  6: 5, // WCQualifier25 range 0-31
-  7: 5, // WCSeries25    range 0-31
-  8: 7, // Cyclone       range 0-127
+  1: 5,   // CoreKS        range 0-31
+  2: 5,   // Core          range 0-31
+  3: 6,   // Alize         range 0-63
+  4: 6,   // Bise          range 0-63
+  5: 6,   // TumultS3      range 0-63
+  6: 5,   // WCQualifier25 range 0-31
+  7: 5,   // WCSeries25    range 0-31
+  8: 7,   // Cyclone       range 0-127
+  9: 7,   // Duster        range 0-127
+  10: 5,  // Duster TOP    range 0-31
+  11: 7,  // Duster CB     range 0-127
+  12: 7,  // Duster OP     range 0-127
 }
+
+export const SetCodeIdWithLegacyRarityLength = [
+  1,  // CoreKS
+  2,  // Core
+  3,  // Alize
+  4,  // Bise
+  5,  // TumultS3
+  6,  // WCQualifier25
+  7,  // WCSeries25
+  8,  // Cyclone
+]
 
 export type CardId = string
 
@@ -62,7 +82,7 @@ export class CardRefElements {
   uniq_num?: number
 
   constructor(id: CardId) {
-    const match = id.match(/^ALT_(\w+)_(A|B|P)_(\w{2})_(\d{1,2})_(C|R1|R2|U)(?:_(\d+))?$/)
+    const match = id.match(/^ALT_(\w+)_(A|B|P)_(\w{2})_(\d{1,2})_(C|R1|R2|U|E)(?:_(\d+))?$/)
     if (!match) { throw "unrecognized card id '" + id + "'" }
 
     this.set_code = (match[1] as RefSetCode)
@@ -103,6 +123,7 @@ export class CardRefElements {
       case RefRarity.Rare: return 1;
       case RefRarity.RareOOF: return 2;
       case RefRarity.Unique: return 3;
+      case RefRarity.Exalt: return 4;
     }
     throw `Unrecognized Rarity ${this.rarity}`
   }
@@ -116,6 +137,10 @@ export class CardRefElements {
       case RefSetCode.WCQualifier25: return 6;
       case RefSetCode.WCSeries25: return 7;
       case RefSetCode.Cyclone: return 8;
+      case RefSetCode.Duster: return 9;
+      case RefSetCode.DusterTOP: return 10;
+      case RefSetCode.DusterCB: return 11;
+      case RefSetCode.DusterOP: return 12;
     }
     throw `Unrecognized SetCode ${this.set_code}`
   }
